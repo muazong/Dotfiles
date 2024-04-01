f5b44f98e825fa84f41638feb6d8fd46388a9f20
muazong <giongmua2004@gmail.com>
2024-04-01 17:46:07 +0700

    update

--- nvim/lua/Plugins/Lsp/P-NvimCmp.lua
+++ nvim/lua/Plugins/Lsp/P-NvimCmp.lua
@@ -52,7 +52,7 @@ cmp.setup({
 		["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
 		["<C-e>"] = cmp.mapping.abort(), -- close completion window
 		["<CR>"] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
+			-- behavior = cmp.ConfirmBehavior.Replace,
 			select = true,
 		}),
 	}),
