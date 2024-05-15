61adbad8c26a434e88e0dd1074562cb822b9e1a0
muazong <giongmua2004@gmail.com>
2024-05-15 17:34:11 +0700

    add hop.nvim plugin

---
+++ nvim/lua/azong/plugins/p-hop.lua
@@ -0,0 +1,9 @@
+return {
+  "phaazon/hop.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  version = "v2",
+  keys = { { "f", "<cmd>HopChar1MW<cr>" } },
+  opts = {
+    keys = "qwerasdfzxcv",
+  },
+}
