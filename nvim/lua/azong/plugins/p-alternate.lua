ecff5ecece3dbadee5f728cc2a8774df8920bf29
muazong <giongmua2004@gmail.com>
2024-05-19 23:07:19 +0700

    change alternate-toggler to nvim-toggler

--- nvim/lua/azong/plugins/p-alternate.lua
+++ nvim/lua/azong/plugins/p-alternate.lua
@@ -1,16 +1,19 @@
 return {
-  --[[ "rmagatti/alternate-toggler",
+  "nguyenvukhang/nvim-toggler",
   event = "BufReadPre",
   config = function()
-    vim.keymap.set("n", "<Leader><Leader>", "<Cmd>ToggleAlternate<CR>")
-    require("alternate-toggler").setup({
-      alternates = {
+    vim.keymap.set("n", "<Leader><Leader>", require("nvim-toggler").toggle)
+    require("nvim-toggler").setup({
+      inverses = {
         ["==="] = "!==",
         ["=="] = "!=",
         ["yes"] = "no",
         ["on"] = "off",
         ["1"] = "0",
+        ["true"] = "false",
       },
+      remove_default_keybinds = true,
+      remove_default_inverses = true,
     })
-  end, ]]
+  end,
 }
