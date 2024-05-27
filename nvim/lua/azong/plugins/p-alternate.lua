8cc303fead8d38585cbeb0085f64c8d19bcd7070
muazong <giongmua2004@gmail.com>
2024-05-27 19:30:24 +0700

    update

--- nvim/lua/azong/plugins/p-alternate.lua
+++ nvim/lua/azong/plugins/p-alternate.lua
@@ -17,6 +17,8 @@ return {
         [">"] = "<",
         ["}"] = "{",
         ["]"] = "[",
+        [")"] = "(",
+        ["+"] = "-",
       },
       remove_default_keybinds = true,
       remove_default_inverses = true,
