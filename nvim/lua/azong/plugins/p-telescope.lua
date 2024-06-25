c8ed238a8afa6728f9e6aa139c725f5a951c7694
muazong <giongmua2004@gmail.com>
2024-06-25 17:09:21 +0700

    update

--- nvim/lua/azong/plugins/p-telescope.lua
+++ nvim/lua/azong/plugins/p-telescope.lua
@@ -17,6 +17,7 @@ return {
     map("n", "<C-g>", "<CMD>Telescope live_grep<CR>", opts)
     map("n", "<C-s>", "<CMD>:Telescope grep_string<CR>", opts)
     map("n", "<C-b>", "<CMD>:Telescope file_browser<CR>", opts)
+    map("n", "tb", "<CMD>:Telescope buffers<CR>", opts)
     map("n", "?", "<CMD>:Telescope help_tags<CR>", opts)
 
     map("n", "<A-f>", "<CMD>Telescope find_files<CR>", opts)
