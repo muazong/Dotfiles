292fa208f0f651a67cd937efa3e299ec317747cb
muazong <giongmua2004@gmail.com>
2024-07-01 22:08:09 +0700

    [Update] - change multicursor mode

--- nvim/lua/azong/plugins/p-multicursor.lua
+++ nvim/lua/azong/plugins/p-multicursor.lua
@@ -2,9 +2,9 @@ return {
   "brenton-leighton/multiple-cursors.nvim",
   opts = {},
   keys = {
-    { "<C-j>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "v" } },
-    { "<C-k>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "v" } },
-    { "<C-d>", "<Cmd>MultipleCursorsAddJumpNextMatch<CR>", mode = { "v" } },
-    { "<S-d>", "<Cmd>MultipleCursorsJumpNextMatch<CR>", mode = { "v" } },
+    { "<C-j>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "v", "x" } },
+    { "<C-k>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "v", "x" } },
+    { "<C-x>", "<Cmd>MultipleCursorsAddJumpNextMatch<CR>", mode = { "v", "x" } },
+    { "<S-x>", "<Cmd>MultipleCursorsJumpNextMatch<CR>", mode = { "v", "x" } },
   },
 }
