8f8ffc22ff1d4cf54627b81a599878c291687775
muazong <giongmua2004@gmail.com>
2024-07-03 16:59:15 +0700

    [Update]

--- nvim/lua/azong/plugins/p-alpha.lua
+++ nvim/lua/azong/plugins/p-alpha.lua
@@ -19,12 +19,12 @@ return {
 
     -- Set menu
     dashboard.section.buttons.val = {
-      dashboard.button("n", "  -> New File", "<cmd>ene<CR>"),
-      dashboard.button("e", "  -> File Explorer", "<cmd>Neotree<CR>"),
-      dashboard.button("f", "󰱼  -> Find File", "<cmd>Telescope find_files<CR>"),
-      dashboard.button("g", "  -> Find Word", "<cmd>Telescope live_grep<CR>"),
-      dashboard.button("l", "󰒲  -> Lazy", "<cmd>Lazy<CR>"),
-      dashboard.button("q", "  -> Quit", "<cmd>qa<CR>"),
+      dashboard.button("n", " 󱦰 New File", "<cmd>ene<CR>"),
+      dashboard.button("e", " 󱦰 File Explorer", "<cmd>Neotree<CR>"),
+      dashboard.button("f", "󰱼 󱦰 Find File", "<cmd>Telescope find_files<CR>"),
+      dashboard.button("g", " 󱦰 Find Word", "<cmd>Telescope live_grep<CR>"),
+      dashboard.button("l", "󰒲 󱦰 Lazy", "<cmd>Lazy<CR>"),
+      dashboard.button("q", " 󱦰 Quit", "<cmd>qa<CR>"),
     }
 
     -- Set footer
