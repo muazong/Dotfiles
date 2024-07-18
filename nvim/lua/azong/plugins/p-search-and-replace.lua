bf93ec62785ae2848b0b5c13912c0704d134f511
muazong <giongmua2004@gmail.com>
2024-07-19 00:58:49 +0700

    [U]

---
+++ nvim/lua/azong/plugins/p-search-and-replace.lua
@@ -0,0 +1,13 @@
+return {
+  "s1n7ax/nvim-search-and-replace",
+  config = function()
+    require("nvim-search-and-replace").setup({
+      ignore = { "**/node_modules/**", "**/.git/**", "**/.gitignore", "**/.gitmodules", "build/**" },
+      update_changes = false,
+      replace_keymap = "<leader>gr",
+      replace_all_keymap = "<leader>gR",
+      replace_and_save_keymap = "<leader>gu",
+      replace_all_and_save_keymap = "<leader>gU",
+    })
+  end,
+}
